3e92382d66db3ad79c67982fa8737672377991c0
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-01-13 22:23:00 -0600

    added flexbox to containers in each component

--- Digital Buisness Card/src/components/body.jsx
+++ Digital Buisness Card/src/components/body.jsx
@@ -1,24 +1,28 @@
 export default function Body(){
     return (
        <main>
-        <section>
+        <section className="container">
+            <div className="container intro">
             <h1> Kenton Jackson </h1>
             <p> Full Stack Developer </p>
             <p> kentonjackson.website </p>
+            </div>
+            <div className="container buttons">
             <a href="https://www.facebook.com/chingoblingers"> Facebook </a>
-            <a href="https://www.linkedin.com/in/kenton-jackson-1b970527b/"> LinkedIn </a> 
-        </section>
+            <a href="https://www.linkedin.com/in/kenton-jackson-1b970527b/"> LinkedIn </a>
+            </div> 
+        
 
-        <section>
-         <p> About </p>
+       
+         <p className="title"> About </p>
             <p> 
                 I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
                 I try to keep up with security and best practices, and am always looking for new things to learn. 
             </p>   
-        </section>
 
-        <section>
-            <p> Interests </p>
+
+       
+            <p className="title"> Interests </p>
                 <p>
                 Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
                 </p>   
