30b7fa694fb50e07dc1e088ae64a3e618275441c
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-01-13 20:58:03 -0600

    installed vite and setup begin structure

---
+++ Digital Buisness Card/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/header'
+import Body from './components/body'
+import Footer from './components/footer'
+
+function App() {
+
+
+  return (
+    <main>
+  <Header />
+  <Body />
+  <Footer />
+    </main>
+  )
+}
+
+export default App
